e0be5f65ca124698956a582877089b79f65a5614
Miguel Leon <migand21@gmail.com>
2020-07-09 07:32:12 -0400

    fix(Auth): importing class and hidding data

--- web/app/Http/Controllers/Api/AuthController.php
+++ web/app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 /**
  * Class AuthController
