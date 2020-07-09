4e71e1f1728e266226290a404fdbabc446c0f118
Pablo Salas <pablo.gun.66@gmail.com>
2020-07-09 14:45:38 -0400

    Revert "Revert "feat(User): Add validation to the user(register and login)""
    This reverts commit 5ee796d4aa375eff568deb72477e7a38439b5214.

--- web/app/Providers/AuthServiceProvider.php
+++ web/app/Providers/AuthServiceProvider.php
@@ -28,5 +28,9 @@ class AuthServiceProvider extends ServiceProvider
 
         // The routes of passport
         Passport::routes();
+
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addDays(7));
+        Passport::tokensExpireIn(Carbon::now()->addDays(7));
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
     }
 }
