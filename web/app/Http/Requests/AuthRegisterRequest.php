2ebda3951fbabcbdad5c654eed65093a6ae24174
Ignacio <ignaciosantanderq12@hotmail.com>
2020-07-15 13:18:47 -0400

    fix(AuthController): Fix validations

--- web/app/Http/Requests/AuthRegisterRequest.php
+++ web/app/Http/Requests/AuthRegisterRequest.php
@@ -24,7 +24,31 @@ class AuthRegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|min:3|max:255',
+            'email' => 'email:rfc,dns|required|unique:users',
+            'password' => 'required|min:8|max:255|confirmed'
         ];
     }
+
+    /**
+     * Get the messages to send when a validation error occur.
+     *
+     * @return array|string[]
+     */
+    public function messages()
+    {
+        return [
+            'name.required'  => 'Se necesita un nombre',
+            'name.min'  => 'Se necesita un mínimo de 3 caracteres en el nombre',
+            'name.max'  => 'Se necesita un máximo de 255 caracteres en el nombre',
+            'email.required'  => 'Se necesita un correo',
+            'email.email'  => 'El correo no es válido',
+            'email.unique'  => 'El correo ya esta en uso',
+            'password.required'  => 'Se necesita una contraseña',
+            'password.min'  => 'Se necesita un mínimo de 3 caracteres en la contraseña',
+            'password.max'  => 'Se necesita un máximo de 255 caracteres en la contraseña',
+        ];
+    }
+
+
 }
