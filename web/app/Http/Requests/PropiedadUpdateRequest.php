4a1de652dcdc1eeb6ff6d1a6434bdcee0f4e8171
Pablo Salas <pablo.gun.66@gmail.com>
2020-07-19 16:21:38 -0400

    feat(PropiedadUpdateRequest): adding some validations to update method in Propiedad

--- web/app/Http/Requests/PropiedadUpdateRequest.php
+++ web/app/Http/Requests/PropiedadUpdateRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class PropiedadUpdateRequest extends FormRequest
 {
@@ -23,8 +25,43 @@ class PropiedadUpdateRequest extends FormRequest
      */
     public function rules()
     {
+        $id = $this->get('id');
+
         return [
-            //
+            'numero' => 'required|integer|unique_numero_update:'.$id,
+            'tipo' => 'in:CASA,DEPARTAMENTO',
+            'comunidad_id' => 'exists:App\Comunidad,id',
         ];
+    }
+
+    /**
+     * Get the messages to send when a validation error occur.
+     *
+     * @return array|string[]
+     */
+    public function messages()
+    {
+        return [
+            'numero.required' => 'Se necesita el numero de la casa',
+            'numero.unique_numero_update' => 'El numero ya esta en uso',
+            'numero.integer' => 'Ingrese un numero entero porfavor',
+            'tipo.in' => 'Porfavor solo ingrese si es una CASA o DEPARTAMENTO',
+            'comunidad_id.exists' => 'El condominio al cual se intento agregar la propiedad no existe',
+        ];
+    }
+
+    /**
+     * FailedValidation [Overriding the event validator for custom error response].
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Validation Error',
+                'error' => $validator->errors()->all()
+            ], 412)
+        );
     }
 }
