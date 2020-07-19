a0f80a8e37c7e41fe55a92ddec98d740f477065c
Ignacio <ignaciosantanderq12@hotmail.com>
2020-07-18 21:08:10 -0400

    feat(Propiedad): Added the controller and the resource

---
+++ web/app/Http/Controllers/Api/PropiedadController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Propiedad;
+use Illuminate\Http\Request;
+
+class PropiedadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Propiedad  $propiedad
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Propiedad $propiedad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Propiedad  $propiedad
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Propiedad $propiedad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Propiedad  $propiedad
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Propiedad $propiedad)
+    {
+        //
+    }
+}
