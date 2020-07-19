2ff98b63ca5176b7842b9c73f84dcf7e77e1627c
Ignacio <ignaciosantanderq12@hotmail.com>
2020-07-19 15:28:09 -0400

    chore(Comunidad): ComunidadRequest Update

--- web/app/Http/Requests/ComunidadStoreRequest.php
+++ web/app/Http/Requests/ComunidadStoreRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ComunidadStoreRequest extends FormRequest
 {
@@ -24,7 +26,41 @@ class ComunidadStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'nombre' => 'required|regex:/^(?!\s*$)[-a-zA-Z]{5,50}$/',
+            'direccion' => 'required|regex:/^(?!\s*$)[-a-zA-Z]{5,50}$/',
+            'tipo' => 'required|in:CONDOMINIO,EDIFICIO',
         ];
+    }
+
+    /**
+     * Get the messages to send when a validation error occur.
+     *
+     * @return array|string[]
+     */
+    public function messages()
+    {
+        return [
+            'nombre.required' => 'Se necesita un nombre',
+            'nombre.regex' => 'Formato no valido, debe contener entre 5 y 50 caracteres',
+            'direccion.required' => 'Se necesita una direccion',
+            'direccion.regex' => 'Formato no valido, debe contener entre 5 y 50 caracteres',
+            'tipo.required' => 'Se necesita un tipo de comunidad',
+            'tipo.in' => 'Puede ser CONDOMINIO o EDIFICIO',
+        ];
+    }
+
+    /**
+     * FailedValidation [Overriding the event validator for custom error response].
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Validation Error',
+                'error' => $validator->errors()->all()
+            ], 412)
+        );
     }
 }
--- web/app/Http/Requests/ComunidadUpdateRequest.php
+++ web/app/Http/Requests/ComunidadUpdateRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ComunidadUpdateRequest extends FormRequest
 {
@@ -24,7 +26,38 @@ class ComunidadUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'nombre' => 'regex:/^(?!\s*$)[-a-zA-Z]{1,50}$/',
+            'direccion' => 'regex:/^(?!\s*$)[-a-zA-Z]{1,50}$/',
+            'tipo' => 'in:CONDOMINIO,EDIFICIO',
         ];
+    }
+
+    /**
+     * Get the messages to send when a validation error occur.
+     *
+     * @return array|string[]
+     */
+    public function messages()
+    {
+        return [
+            'nombre.regex' => 'Formato no valido, debe contener entre 5 y 50 caracteres',
+            'direccion.regex' => 'Formato no valido, debe contener entre 5 y 50 caracteres',
+            'tipo.in' => 'Puede ser CONDOMINIO o EDIFICIO',
+        ];
+    }
+
+    /**
+     * FailedValidation [Overriding the event validator for custom error response].
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Validation Error',
+                'error' => $validator->errors()->all()
+            ], 412)
+        );
     }
 }
