cb274a77b1ff9d25e7a52d7a837efc954664e5e7
Ignacio <ignaciosantanderq12@hotmail.com>
2020-07-18 21:07:09 -0400

    feat(Comunidad): Added the controller and the resource

---
+++ web/app/Http/Controllers/Api/ComunidadController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Comunidad;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ComunidadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Comunidad  $comunidad
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comunidad $comunidad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Comunidad  $comunidad
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comunidad $comunidad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Comunidad  $comunidad
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comunidad $comunidad)
+    {
+        //
+    }
+}
