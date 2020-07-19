6ca0fa656e12a309ead883a41496f52dcd77e091
Ignacio <ignaciosantanderq12@hotmail.com>
2020-07-18 21:08:59 -0400

    feat(Visita): Added the controller and the resource

---
+++ web/app/Http/Controllers/Api/VisitaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Visita;
+use Illuminate\Http\Request;
+
+class VisitaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Visita  $visita
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Visita $visita)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Visita  $visita
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Visita $visita)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Visita  $visita
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Visita $visita)
+    {
+        //
+    }
+}
