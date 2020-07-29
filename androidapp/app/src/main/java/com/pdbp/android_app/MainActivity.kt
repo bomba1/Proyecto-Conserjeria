307cade7742899940b41e1ab1d81efbdabbccf93
Miguel Leon <migand21@gmail.com>
2020-07-29 19:09:09 -0400

    feat(MainActivity): UI initial implementation

--- androidapp/app/src/main/java/com/pdbp/android_app/MainActivity.kt
+++ androidapp/app/src/main/java/com/pdbp/android_app/MainActivity.kt
@@ -1,22 +1,71 @@
 package com.pdbp.android_app
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.getValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
+import androidx.ui.foundation.AdapterList
 import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import androidx.ui.layout.Row
+import androidx.ui.layout.padding
+import androidx.ui.livedata.observeAsState
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import com.pdbp.android_app.data.Persona
 import com.pdbp.android_app.ui.AndroidappTheme
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel> {
+        object : ViewModelProvider.Factory{
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return MainViewModel(apiRestEndPoints) as T
+            }
+
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidappTheme {
-                Greeting("Android")
+                Personas(personasData = viewModel.personasData)
             }
         }
+
+        viewModel.getPersonas()
     }
+}
+
+
+@Composable
+fun Personas(personasData: LiveData<List<Persona>>){
+    val personas by personasData.observeAsState(emptyList())
+    AdapterList(
+        data = personas
+    ) { persona ->
+        PersonaItem(persona = persona)
+    }
+}
+
+@Composable
+fun PersonaItem(persona: Persona) {
+    Column(
+    ) {
+        Text(text = persona.rut)
+        Text(text = persona.nombre)
+        Text(text = persona.telefono)
+        Text(text = persona.email)
+    }
+
 }
 
 @Composable
