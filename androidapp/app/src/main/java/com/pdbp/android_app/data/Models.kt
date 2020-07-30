26271409b81072689cc7757223fb2eab58cfd8ba
Pablo Salas <pablo.gun.66@gmail.com>
2020-07-30 01:58:01 -0400

    feat(Visita): Creation of model Visita

--- androidapp/app/src/main/java/com/pdbp/android_app/data/Models.kt
+++ androidapp/app/src/main/java/com/pdbp/android_app/data/Models.kt
@@ -1,16 +1,33 @@
 package com.pdbp.android_app.data
 
 
+/**
+ * Class that contains a list of Propiedad from json
+ */
 data class Propiedades(
     val message: String,
     val propiedades: List<Propiedad>
 )
 
+/**
+ * Class that contains a list of Personafrom json
+ */
 data class Personas(
     val message: String,
     val personas: List<Persona>
 )
 
+/**
+ * Class that contains a list of Visita from json
+ */
+data class Visitas(
+    val message: String,
+    val visitas: List<Visita>
+)
+
+/**
+ * Class that represents the model of Persona
+ */
 data class Persona(
     val id: Int,
     val rut: String,
@@ -19,6 +36,9 @@ data class Persona(
     val email: String
 )
 
+/**
+ * Class that represents the model of Propiedad
+ */
 data class Propiedad(
     val id: Int,
     val numero: Int,
@@ -26,3 +46,14 @@ data class Propiedad(
     val comunidad_id: Int
 )
 
+/**
+ * Class that represents the model of Visita
+ */
+data class Visita(
+    val id: Int,
+    val fecha: String,
+    val parentesco: String,
+    val empresa_reparto: String,
+    val persona_id: Int,
+    val propiedad_id: Int
+)
